f4067576dd77068720870b4326cc317547661364
alexis77alfaro <ga0382032018@unab.edu.sv>
2021-07-12 20:19:45 -0600

    Primer push

---
+++ src/main/java/com/visualOptic/VisualOpticApplication.java
@@ -0,0 +1,13 @@
+package com.visualOptic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualOpticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualOpticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visualOptic/VisualOpticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visualOptic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualOpticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
